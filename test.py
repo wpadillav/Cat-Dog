202b9d6e30f12e682800e19ec5939c398e8797be
wpadillav <william.padilla@uniminuto.edu.co>
2025-06-04 22:47:08 -0500

    first commit

--- test.py
+++
@@ -1,7 +0,0 @@
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-
-import tensorflow as tf
-
-print("TensorFlow version:", tf.__version__)
-print("GPUs available:", tf.config.list_physical_devices('GPU'))
